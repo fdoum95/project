ca66fc1961c78a83f10f9d72fabcb617e2fdef66
dimitriskon95 <konstantakis.dim@gmail.com>
2016-10-26 00:00:26 +0300

    Add files via linux

--- System.cpp
+++ System.cpp
@@ -1,15 +1,17 @@
 #include "System.h"
 #include "Hamming.h" 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-System::System(char metric, int k, int L)
+System::System(char m, int k, int L, int bit_size)
 {
-	if (metric = 'H')
+	metric = m;
+	if (metric == 'H')
 	{
-		hamming = new Hamming(k, L);
 		cout << "A new system was created with hamming metric." << endl;
+		hamming = new Hamming(k, L, bit_size);
 	}
 }
 
@@ -17,3 +19,73 @@ System::~System()
 {
 	delete hamming;
 }
+
+
+void System::Menu()
+{
+	string option;
+	cout << "You have entered the Menu Control" << endl;
+	while(1)
+	{
+		Display();
+		cout << "Give an option" << endl;
+		cin >> option;
+		cout << "Do you want to terminate the program (Y/N)" << endl;
+		cin >> option;
+		if (option == "Y")
+		{
+			cout << "Exit Menu control" << endl;
+			break;
+		}
+	}
+}
+
+void System::Random_Generator(char metric, int size)
+{
+	if (metric == 'H')
+	{
+		hamming->Random_Simple(size);
+	}
+	else if (metric == 'E')
+	{
+		
+	}
+	else if (metric = 'C')
+	{
+		
+	}
+}
+
+void System::Display()
+{
+	hamming->Display();
+}
+
+void System::Reader(char *line)
+{
+	int line_count = 0;
+	char *bstring;
+	bstring = new char[hamming->bsize_return()];
+	if (metric == 'H')
+	{
+		while(line[line_count] != '\t')
+			line_count++;
+		line_count++;
+		
+		strcpy(bstring, &line[line_count]);  //copy bit string from line to bstring
+		cout << bstring << endl;
+		
+		//new node
+		//Insert node at list
+	}
+	else if (metric == 'E')
+	{
+		
+	}
+	else if (metric = 'C')
+	{
+		
+	}
+}
+
+
--- System.h
+++ System.h
@@ -1,17 +1,27 @@
 #ifndef System_H
 #define System_H
 
-#include "Hamming.h" 
+#include "Hamming.h"
+#include <string.h> 
 
 class System
 {
 	private:
+		char metric;
 		Hamming *hamming;
 		
 		
 	public:
-		System(char , int, int);
+		System(char , int, int, int);
 		~System();
+		
+		void Menu();
+		
+		void Random_Generator(char, int);
+		
+		void Display();
+		
+		void Reader(char *);
 };
 
 #endif
